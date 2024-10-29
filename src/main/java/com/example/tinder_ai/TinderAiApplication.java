b7f8d58d4db5008abd3f22ceba3faa1d3ec1490f
PC <jupallyyashwanth376@gmail.com>
2024-10-29 18:53:11 +0530

    Initial commit

---
+++ src/main/java/com/example/tinder_ai/TinderAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinder_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tinder_ai/TinderAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinder_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
